aa4761dde47a658e26b5be673f679997b9ce95c6
Yigal Korman <ykorman@gmail.com>
2013-11-30 13:08:14 +0200

    moving to page action instead of browser action

--- background.js
+++ background.js
@@ -1,8 +1,13 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
+// Called when the user clicks on the page action.
+chrome.pageAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   console.log('Opening next results page of ' + tab.url);
   chrome.tabs.executeScript({
     code: 'document.getElementById("pnnext").click();'
   });
+});
+
+// register the page action on all pages
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  chrome.pageAction.show(tabId);
 });
