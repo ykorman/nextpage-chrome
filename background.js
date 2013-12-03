8946fbb462dcdfef356270406243a882c5a27d40
Yigal Korman <ykorman@gmail.com>
2013-12-03 20:23:55 +0200

    adding support for arstechnica.com

--- background.js
+++ background.js
@@ -7,9 +7,54 @@ chrome.pageAction.onClicked.addListener(function(tab) {
   });
 });
 
+var emptyFunction = function() {}
+
+var execCommand = emptyFunction;
+
 // Listen for the content script (element_searcher.js) to send a message to the
 // background page.
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log("got message " + request);
-  chrome.pageAction.show(sender.tab.id);
+  switch (request) {
+    case "google-search":
+      execCommand = execGoogleSearchCommand;
+      chrome.pageAction.show(sender.tab.id);
+      break;
+    case "arstechnica":
+      execCommand = execArsTechnicaCommand;
+      chrome.pageAction.show(sender.tab.id);
+      break;
+    case "none":
+      execCommand = emptyFunction;
+      if (sender.tab != undefined)
+        chrome.pageAction.hide(sender.tab.id);
+      break;
+    default:
+      break;
+  }
+  
+});
+
+function clickOnElementById(id) {
+  chrome.tabs.executeScript(
+    {code: 'document.getElementById("' + id + '").click();'}
+  );
+}
+
+function clickOnElementByClass(className) {
+  chrome.tabs.executeScript(
+    {code: 'document.getElementsByClassName("' + className + '")[0].click();'}
+  );
+}
+
+function execGoogleSearchCommand(command) {
+  clickOnElementById("pnnext");
+}
+
+function execArsTechnicaCommand(command) {
+  clickOnElementByClass("next");
+}
+
+// shortcut commands
+chrome.commands.onCommand.addListener(function(command) {
+  execCommand(command);
 });
--- element_searcher.js
+++ element_searcher.js
@@ -1,5 +1,9 @@
 (function () {
   if (document.getElementById("pnnext") !== null) {
-    chrome.runtime.sendMessage("google_search");
+    chrome.runtime.sendMessage("google-search");
+  } else if (document.getElementsByClassName("next")[0] !== undefined) {
+    chrome.runtime.sendMessage("arstechnica");
+  } else {
+    chrome.runtime.sendMessage("none");
   }
 })();
