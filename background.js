2fc02c3d99ad8c84d4c7163b49a7b548ec029439
Yigal Korman <ykorman@gmail.com>
2013-12-03 22:01:11 +0200

    mechanism redsign

--- background.js
+++ background.js
@@ -1,60 +1,33 @@
-// Called when the user clicks on the page action.
-chrome.pageAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Opening next results page of ' + tab.url);
-  chrome.tabs.executeScript({
-    code: 'document.getElementById("pnnext").click();'
-  });
-});
-
-var emptyFunction = function() {}
-
-var execCommand = emptyFunction;
+var nextCommandQuery = "";
 
 // Listen for the content script (element_searcher.js) to send a message to the
 // background page.
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  switch (request) {
-    case "google-search":
-      execCommand = execGoogleSearchCommand;
+  if (request === "none") {
+    nextCommandQuery = "";
+    if (sender.tab != undefined)
+          chrome.pageAction.hide(sender.tab.id);
+  } else {
+    if (sender.tab != undefined)
       chrome.pageAction.show(sender.tab.id);
-      break;
-    case "arstechnica":
-      execCommand = execArsTechnicaCommand;
-      chrome.pageAction.show(sender.tab.id);
-      break;
-    case "none":
-      execCommand = emptyFunction;
-      if (sender.tab != undefined)
-        chrome.pageAction.hide(sender.tab.id);
-      break;
-    default:
-      break;
+    nextCommandQuery = request;
   }
-  
 });
 
-function clickOnElementById(id) {
+function clickOnElementByQuery(query) {
   chrome.tabs.executeScript(
-    {code: 'document.getElementById("' + id + '").click();'}
+    {code: 'document.querySelector("' + query + '").click();'}
   );
-}
-
-function clickOnElementByClass(className) {
-  chrome.tabs.executeScript(
-    {code: 'document.getElementsByClassName("' + className + '")[0].click();'}
-  );
-}
-
-function execGoogleSearchCommand(command) {
-  clickOnElementById("pnnext");
-}
-
-function execArsTechnicaCommand(command) {
-  clickOnElementByClass("next");
 }
 
 // shortcut commands
 chrome.commands.onCommand.addListener(function(command) {
-  execCommand(command);
+  if (nextCommandQuery !== "")
+    clickOnElementByQuery(nextCommandQuery);
+});
+
+// Called when the user clicks on the page action.
+chrome.pageAction.onClicked.addListener(function(tab) {
+  if (nextCommandQuery !== "")
+    clickOnElementByQuery(nextCommandQuery);
 });
--- element_searcher.js
+++ element_searcher.js
@@ -1,9 +1,29 @@
 (function () {
-  if (document.getElementById("pnnext") !== null) {
-    chrome.runtime.sendMessage("google-search");
-  } else if (document.getElementsByClassName("next")[0] !== undefined) {
-    chrome.runtime.sendMessage("arstechnica");
-  } else {
-    chrome.runtime.sendMessage("none");
+
+  var selector = {};
+  
+  selector["google_search"] = "#pnnext";
+  selector["arstechnica"] = "span.next";
+  
+  // keep always as last
+  // in arstechnica this one moves to the next article, and the
+  // more specific above moves to next page (if exists)
+  selector["general"] = "a[rel=next]";
+  
+  var found = false;
+  
+  for (site in selector) {
+    if (selector.hasOwnProperty(site)) {
+      var query = selector[site];
+      if (document.querySelector(query) !== null) {
+        found = true;
+        chrome.runtime.sendMessage(query);
+        break;
+      }
+    }
   }
+  
+  if (!found)
+    chrome.runtime.sendMessage("none");
+  
 })();
