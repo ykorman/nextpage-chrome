223a91e75f3799041dfc0b906fd0dae12b1be231
Yigal Korman <ykorman@gmail.com>
2013-12-31 21:13:47 +0200

    added a "tab" for namespace

--- background.js
+++ background.js
@@ -1,56 +1,56 @@
 (function () {  // namespace protection
 
-var nextCommandQueryMap = {};
+  var nextCommandQueryMap = {};
 
-// Listen for the content script (element_searcher.js) to send a message to the
-// background page.
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request === "none") {
-    if ((sender.tab !== undefined) && (sender.tab.id in nextCommandQueryMap))
-      console.log("got null request for tab " + sender.tab.id + 
-                  "which previously held \'" + 
-                  nextCommandQueryMap[sender.tab.id] + "\'"); 
-    nextCommandQueryMap[sender.tab.id] = "";
-    if (sender.tab !== undefined)
-      chrome.pageAction.hide(sender.tab.id);
-  } else {
-    console.log("got identification of \'" + request + 
-                "\' for tab " + sender.tab.id);
-    if (sender.tab !== undefined)
-      chrome.pageAction.show(sender.tab.id);
-    nextCommandQueryMap[sender.tab.id] = request;
+  // Listen for the content script (element_searcher.js) to send a message to the
+  // background page.
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request === "none") {
+      if ((sender.tab !== undefined) && (sender.tab.id in nextCommandQueryMap))
+        console.log("got null request for tab " + sender.tab.id + 
+                    "which previously held \'" + 
+                    nextCommandQueryMap[sender.tab.id] + "\'"); 
+      nextCommandQueryMap[sender.tab.id] = "";
+      if (sender.tab !== undefined)
+        chrome.pageAction.hide(sender.tab.id);
+    } else {
+      console.log("got identification of \'" + request + 
+                  "\' for tab " + sender.tab.id);
+      if (sender.tab !== undefined)
+        chrome.pageAction.show(sender.tab.id);
+      nextCommandQueryMap[sender.tab.id] = request;
+    }
+  });
+
+  function injectedCallback(results) {
+    // TODO handle results
+    // console.log("got result back: " + results);
   }
-});
 
-function injectedCallback(results) {
-  // TODO handle results
-  // console.log("got result back: " + results);
-}
-
-function clickOnElementByQuery(query) { 
-  // TODO move injected code to separate file, use file: instead of code:
-  chrome.tabs.executeScript(null,
-    {code: 'document.querySelector("' + query + '").click();'},
-    injectedCallback);
-}
-
-// shortcut commands
-chrome.commands.onCommand.addListener(function(command) {
-  console.log("got command " + command);
-  if (command == "open-next-page") {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var current = tabs[0];
-      clickOnElementByQuery(nextCommandQueryMap[current.id]);
-    });
-  } else if (command == "open-prev-page") {
-    chrome.tabs.executeScript(null, {code: 'history.back();'});
+  function clickOnElementByQuery(query) { 
+    // TODO move injected code to separate file, use file: instead of code:
+    chrome.tabs.executeScript(null,
+      {code: 'document.querySelector("' + query + '").click();'},
+      injectedCallback);
   }
-});
 
-// Called when the user clicks on the page action.
-chrome.pageAction.onClicked.addListener(function(tab) {
-  if (nextCommandQueryMap[tab.id] !== "")
-    clickOnElementByQuery(nextCommandQueryMap[tab.id]);
-});
+  // shortcut commands
+  chrome.commands.onCommand.addListener(function(command) {
+    console.log("got command " + command);
+    if (command == "open-next-page") {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var current = tabs[0];
+        clickOnElementByQuery(nextCommandQueryMap[current.id]);
+      });
+    } else if (command == "open-prev-page") {
+      chrome.tabs.executeScript(null, {code: 'history.back();'});
+    }
+  });
+
+  // Called when the user clicks on the page action.
+  chrome.pageAction.onClicked.addListener(function(tab) {
+    if (nextCommandQueryMap[tab.id] !== "")
+      clickOnElementByQuery(nextCommandQueryMap[tab.id]);
+  });
 
 })(); // namespace protection end
